58f65858804b7a9ee590ec6d3e68717ba6f0d0a8
Joseph Donaldson <donaldsonjw@yahoo.com>
2015-01-17 09:16:54 -0800

    Synced with bigloo4.2a-alpha13Jan15

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Tue Jan 6 16:22:48 CET 2015 ")
+(define (bigloo-date)  " Sun Jan 11 21:05:31 CET 2015 ")
