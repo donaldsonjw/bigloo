5bd3c4bd9069233b26ad7655fc7a700f7b5ddb9d
Joseph Donaldson <donaldsonjw@yahoo.com>
2015-03-07 09:02:19 -0800

    Merged updated Bigloo from upstream

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Wed Feb 18 07:35:11 CET 2015 ")
+(define (bigloo-date)  " Sat Feb 21 15:16:07 PST 2015 ")
