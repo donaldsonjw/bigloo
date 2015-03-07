9550efde06df3ec595383e2a236cb4c88e9731bc
Joseph Donaldson <donaldsonjw@yahoo.com>
2015-03-07 12:21:28 -0800

    Preventing building of documentation

--- comptime/Tools/date.scm
+++ comptime/Tools/date.scm
@@ -1,4 +1,4 @@
 ;; Automatically generated file (don't edit)
 (module tools_date (export (bigloo-date)))
 
-(define (bigloo-date)  " Sat Mar 7 10:41:51 PST 2015 ")
+(define (bigloo-date)  " Sat Mar 7 12:06:46 PST 2015 ")
