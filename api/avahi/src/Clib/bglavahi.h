c19242198867fbde8f4c9241c70f35c6d2bb0863
Joseph Donaldson <donaldsonjw@yahoo.com>
2015-11-21 10:54:54 -0800

    Sync to Bigloo 4.2b final

--- api/avahi/src/Clib/bglavahi.h
+++ api/avahi/src/Clib/bglavahi.h
@@ -1,9 +1,9 @@
 /*===========================================================================*/
 /*   (Llib/avahi.scm)                                                        */
 /*   Bigloo (4.2b)                                                           */
-/*   Inria -- Sophia Antipolis (c)       Thu Oct 8 14:10:04 CEST 2015        */
+/*   Inria -- Sophia Antipolis (c)       Tue Oct 27 15:44:41 CET 2015        */
 /*===========================================================================*/
-/* COMPILATION: (/home/serrano/prgm/project/bigloo/bin/bigloo -O3 -fcfa-arithmetic -q -I Llib -lib-dir /home/serrano/prgm/project/bigloo/lib/bigloo/4.2b -unsafe -safee -srfi avahi -copt -D_REENTRANT -copt -fPIC -copt -IClib -copt  Llib/avahi.scm -o Clib/bglavahi.h -hgen)*/
+/* COMPILATION: (/home/serrano/prgm/project/bigloo/bin/bigloo -O3 -fcfa-arithmetic -q -lib-dir /home/serrano/prgm/project/bigloo/lib/bigloo/4.2b -I Llib -lib-dir /home/serrano/prgm/project/bigloo/lib/bigloo/4.2b -unsafe -safee -srfi avahi -copt -D_REENTRANT -copt -fPIC -copt -IClib -copt  Llib/avahi.scm -o Clib/bglavahi.h -hgen)*/
 
 /* Object type definitions */
 typedef struct BgL_z62avahizd2errorzb0_bgl {
